959c32553bc0533e4c8d3b5cb6913252f542be9d
wsdf25867 <wsdf25867@gmail.com>
2024-09-30 21:04:47 +0900

    Initial commit

---
+++ src/main/kotlin/io/clean/special_lecture/SpecialLectureApplication.kt
@@ -0,0 +1,11 @@
+package io.clean.special_lecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpecialLectureApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpecialLectureApplication>(*args)
+}
---
+++ src/test/kotlin/io/clean/special_lecture/SpecialLectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.clean.special_lecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpecialLectureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
