ab6bb292df80635220bc7340ad82d6e5a5c0859d
wsdf25867 <wsdf25867@gmail.com>
2024-10-03 16:05:16 +0900

    refactor: 테스트 fixture 생성 파일 분리

--- src/test/kotlin/io/clean/special_lecture/domain/special_lecture/SpecialLectureTest.kt
+++ src/test/kotlin/io/clean/special_lecture/domain/special_lecture/SpecialLectureTest.kt
@@ -69,24 +69,4 @@ class SpecialLectureTest {
         // then
         assertThat(specialLecture.lecturers).hasSize(1)
     }
-
-    private fun createLecture(
-        title: String = DEFAULT_TITLE,
-        enrollStartDateTime: LocalDateTime = DEFAULT_START_DATE_TIME,
-        enrollEndDateTime: LocalDateTime = DEFAULT_END_DATE_TIME,
-        capacity: Int = DEFAULT_CAPACITY,
-    ): SpecialLecture = SpecialLecture(
-        title = title,
-        enrollStartDateTime = enrollStartDateTime,
-        enrollEndDateTime = enrollEndDateTime,
-        capacity = capacity,
-    )
-
-
-    companion object {
-        private const val DEFAULT_TITLE = "title"
-        private const val DEFAULT_CAPACITY = 30
-        private val DEFAULT_START_DATE_TIME = LocalDateTime.of(1995, 3, 26, 10, 0)
-        private val DEFAULT_END_DATE_TIME = DEFAULT_START_DATE_TIME.plusDays(1)
-    }
 }
